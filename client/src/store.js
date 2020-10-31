4e79e7d3ed901c95e115a691450dd27226740373
Michael Timmons <michael.timmons@protonmail.com>
2020-10-31 17:21:39 -0400

    clean up

--- client/src/store.js
+++ client/src/store.js
@@ -2,7 +2,6 @@ import { configureStore, createReducer } from '@reduxjs/toolkit'
 
 const rangeReducer = createReducer({hero:[],villian:[]}, {
     HERO_RANGE: (state, action) => {
-      // "mutate" the array by calling push()
       state.hero = action.payload
     },
     VILLIAN_RANGE: (state, action) => {
