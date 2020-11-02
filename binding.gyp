5d04e3253c0656a2816a7d059346c28e75a1fd40
Michael Timmons <michael.timmons@protonmail.com>
2020-11-01 19:12:20 -0500

    update bindings

--- binding.gyp
+++ binding.gyp
@@ -4,8 +4,14 @@
       "target_name": "hello",
       "sources": [ "hello.cc" ],
       "include_dirs": [
-        "<!(node -e \"require('nan')\")"
-      ]
+        "<!(node -e \"require('nan')\")",
+        "/home/michael/training-app/pokerstove/src/lib"
+      ],
+      "libraries":[
+        "/home/michael/training-app/pokerstove/build/src/lib/pokerstove/penum/libpenum.a",
+        "/home/michael/training-app/pokerstove/build/src/lib/pokerstove/peval/libpeval.a",
+      ],
+      'cflags_cc': [ '-fexceptions' ]
     }
   ]
 }
--- hello.cc
+++ hello.cc
@@ -1,16 +1,29 @@
 #include <nan.h>
+#include <boost/program_options.hpp>
+#include <pokerstove/penum/ShowdownEnumerator.h>
+#include <pokerstove/peval/CardSet.h>
+#include <vector>
+#include <string>
 
-void Method(const Nan::FunctionCallbackInfo<v8::Value>& info) {
+using namespace std;
+void Method(const Nan::FunctionCallbackInfo<v8::Value> &info)
+{
   info.GetReturnValue().Set(Nan::New("world").ToLocalChecked());
 }
 
-void Init(v8::Local<v8::Object> exports) {
+void Init(v8::Local<v8::Object> exports)
+{
   v8::Local<v8::Context> context = exports->CreationContext();
   exports->Set(context,
                Nan::New("hello").ToLocalChecked(),
                Nan::New<v8::FunctionTemplate>(Method)
                    ->GetFunction(context)
                    .ToLocalChecked());
+  const string game = string("h");
+  vector<pokerstove::CardDistribution> handDists;
+  handDists.emplace_back();
+  handDists.back().parse("AcAs,KcKs");
+  printf("The value of a : %zu", handDists.back().size());
 }
 
 NODE_MODULE(hello, Init)
