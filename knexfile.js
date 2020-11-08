dee273ca2847d7b04a2672b8d44640dcea73f96a
Michael Timmons <michael.timmons@protonmail.com>
2020-11-08 01:17:23 -0500

    feat(api) Add knex

---
+++ knexfile.js
@@ -0,0 +1,25 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.sqlite3'
+    },
+    useNullAsDefault: true
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool:{
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tablename: 'knex_migrations',
+      directory: "./migrations"
+    }
+  }
+
+};
